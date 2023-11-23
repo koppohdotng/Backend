015e4e410da41d2e6d6ee486be02638c03dd4b02
Timmy LawaL <t.lawal@alphaafrican.com>
2023-11-23 21:08:20 +0100

    2023/11/23

--- routes/auth.js
+++ routes/auth.js
@@ -111,7 +111,7 @@ router.get('/login', (req, res) => {
       // Check Firebase authentication
       const userRecord = await admin.auth().getUserByEmail(email);
       { message: "Login successful" }
-      res.status(200).json({ message: "Login successful", user: userRecord.uid  });
+      res.status(200).json({ message: "Login successful", user: userRecord.user});
 
     } catch (error) {
       console.error('Firebase authentication error:', error);
