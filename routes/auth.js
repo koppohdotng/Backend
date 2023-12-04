71282da7bd4194f9bfda7440b131d144a1aeae5f
Timmy LawaL <t.lawal@alphaafrican.com>
2023-12-04 09:46:20 +0100

    2023/12/4/

--- routes/auth.js
+++ routes/auth.js
@@ -95,28 +95,28 @@ router.post('/signup', (req, res) => {
       });
 });
 
-const authenticateUser = (req, res, next) => {
-  const userId = req.headers.userid;
+// const authenticateUser = (req, res, next) => {
+//   const userId = req.headers.userid;
 
-  if (!userId) {
-    return res.status(401).json({ error: 'Unauthorized' });
-  }
+//   if (!userId) {
+//     return res.status(401).json({ error: 'Unauthorized' });
+//   }
 
-  // Check if the user exists or perform any additional checks based on your requirements
-  // You might want to store user data in a Firestore collection or another database
+//   // Check if the user exists or perform any additional checks based on your requirements
+//   // You might want to store user data in a Firestore collection or another database
 
-  req.user = { uid: userId };
-  next();
-};
+//   req.user = { uid: userId };
+//   next();
+// };
 
-// Logout endpoint
-app.post('/logout', authenticateUser, (req, res) => {
-  // Perform any additional cleanup or session management if needed
-  const { uid } = req.user;
-  // You may want to update your database or perform any necessary actions on logout
+// // Logout endpoint
+// app.post('/logout', authenticateUser, (req, res) => {
+//   // Perform any additional cleanup or session management if needed
+//   const { uid } = req.user;
+//   // You may want to update your database or perform any necessary actions on logout
 
-  res.json({ message: `Logout successful for user ${uid}` });
-});
+//   res.json({ message: `Logout successful for user ${uid}` });
+// });
 
 
 router.get('/login', (req, res) => {
