87970b4791bdf449f1a1c1621d96a074b21dc117
Timmy LawaL <t.lawal@alphaafrican.com>
2023-12-07 19:59:26 +0100

    2023/12/7

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const authRoutes = require('./routes/auth');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 const path = require('path');
+const wss = new WebSocket.Server({ server });
 // const verifyGoogleIdToken = require('./google-signin');
 
 
@@ -21,6 +22,46 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
   res.send('Hello, Express!');
 });
+
+app.post('/storeChat', (req, res) => {
+  const { userId, fundingRequestId, newChat, sourceChat } = req.body;
+
+  if (!userId || !fundingRequestId || !newChat || !sourceChat) {
+    return res.status(400).json({ error: 'Missing required parameters' });
+  }
+
+  const database = admin.database();
+  const fundingRequestRef = database.ref(`users/${userId}/fundingRequest/${fundingRequestId}/chats`);
+
+  const chatData = {
+    date: new Date().toISOString(),
+    message: newChat,
+    source: sourceChat
+  };
+
+  fundingRequestRef.push(chatData, (error) => {
+    if (error) {
+      return res.status(500).json({ error: 'Failed to store chat data' });
+    }
+
+    return res.status(200).json({ success: true });
+  });
+});
+
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
+    // Handle incoming WebSocket messages
+    const data = JSON.parse(message);
+
+    // Broadcast the message to all connected clients
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(data));
+      }
+    });
+  });
+});
+
 
 app.get('/api/user/:userId', (req, res) => {
   const userId = req.params.userId;
