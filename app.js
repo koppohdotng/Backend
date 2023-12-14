8bc8f153a0336d3deb3bc18ae54e3ecda55c4892
Timmy LawaL <t.lawal@alphaafrican.com>
2023-12-14 23:57:10 +0100

    2023/12/13

--- app.js
+++ app.js
@@ -1040,7 +1040,7 @@ app.post('/api/uploadReceipt/:userId', upload.single('receipt'), (req, res) => {
 
   // Create a new receipt object
   const newReceipt = {
-    date,
+    dateReceipt,
     type,
     receiptURL: '', // Initialize the receiptURL field
   };
