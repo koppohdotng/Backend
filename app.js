306189483b31918d94e43f7836cf2969f60295a8
Timmy LawaL <t.lawal@alphaafrican.com>
2023-12-31 18:05:52 +0100

    2023/12/30

--- app.js
+++ app.js
@@ -648,8 +648,10 @@ app.post('/loanRequest/:userId', upload.fields([
           // Retrieve the saved data using the key
                 const savedData = fundingRequest[newKey];
 
-          res.status(200).json({ message: 'Loan request data updated successfully.'},
-          {savedData: savedData});
+          res.status(200).json({
+            message: 'Loan request data updated successfully.',
+            savedData: savedData
+          });
         }
       });
     })
