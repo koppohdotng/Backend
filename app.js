b818d8ebb32f55574168e8342f6b43895689c9df
koppohdotng <info.koppoh@gmail.com>
2024-01-26 13:30:06 +0100

    sentry

--- app.js
+++ app.js
@@ -12,48 +12,48 @@ var postmark = require("postmark");
 var client = new postmark.ServerClient("61211298-3714-4551-99b0-1164f8a9cb33");
 const fs = require('fs');
 
-const Sentry = require('@sentry/node');
-const { init } = require('@sentry/tracing');
+// const Sentry = require('@sentry/node');
+// const { init } = require('@sentry/tracing');
 
 
-Sentry.init({
-  dsn: "https://fcdfd51065d11d6a93d2a82b8d323961@o4506625417216000.ingest.sentry.io/4506626630615040",
-  integrations: [
-    // enable HTTP calls tracing
-    new Sentry.Integrations.Http({ tracing: true }),
-    // enable Express.js middleware tracing
-    new Sentry.Integrations.Express({ app }),
-    new ProfilingIntegration(),
-  ],
-  // Performance Monitoring
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set sampling rate for profiling - this is relative to tracesSampleRate
-  profilesSampleRate: 1.0,
-});
+// Sentry.init({
+//   dsn: "https://fcdfd51065d11d6a93d2a82b8d323961@o4506625417216000.ingest.sentry.io/4506626630615040",
+//   integrations: [
+//     // enable HTTP calls tracing
+//     new Sentry.Integrations.Http({ tracing: true }),
+//     // enable Express.js middleware tracing
+//     new Sentry.Integrations.Express({ app }),
+//     new ProfilingIntegration(),
+//   ],
+//   // Performance Monitoring
+//   tracesSampleRate: 1.0, //  Capture 100% of the transactions
+//   // Set sampling rate for profiling - this is relative to tracesSampleRate
+//   profilesSampleRate: 1.0,
+// });
 
-app.use(Sentry.Handlers.requestHandler());
+// app.use(Sentry.Handlers.requestHandler());
 
-// TracingHandler creates a trace for every incoming request
-app.use(Sentry.Handlers.tracingHandler());
+// // TracingHandler creates a trace for every incoming request
+// app.use(Sentry.Handlers.tracingHandler());
 
-app.get("/", function rootHandler(req, res) {
-  res.end("Hello world!");
-});
+// app.get("/", function rootHandler(req, res) {
+//   res.end("Hello world!");
+// });
 
-// The error handler must be registered before any other error middleware and after all controllers
-app.use(Sentry.Handlers.errorHandler());
+// // The error handler must be registered before any other error middleware and after all controllers
+// app.use(Sentry.Handlers.errorHandler());
 
-// Optional fallthrough error handler
-app.use(function onError(err, req, res, next) {
-  // The error id is attached to `res.sentry` to be returned
-  // and optionally displayed to the user for support.
-  res.statusCode = 500;
-  res.end(res.sentry + "\n");
-});
+// // Optional fallthrough error handler
+// app.use(function onError(err, req, res, next) {
+//   // The error id is attached to `res.sentry` to be returned
+//   // and optionally displayed to the user for support.
+//   res.statusCode = 500;
+//   res.end(res.sentry + "\n");
+// });
 
-app.get("/debug-sentry", function mainHandler(req, res) {
-  throw new Error("My first Sentry error!");
-});
+// app.get("/debug-sentry", function mainHandler(req, res) {
+//   throw new Error("My first Sentry error!");
+// });
 
 
 
