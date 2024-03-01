3e395db8bbbe61377da9e35ed6a2f89f31a60e08
koppohdotng <info.koppoh@gmail.com>
2024-03-01 16:48:15 +0100

    add new  admins

--- admin/adminRoutes.js
+++ admin/adminRoutes.js
@@ -75,8 +75,48 @@ router.post('/loginAdmin', async (req, res) => {
   else{
 
     if (admin.passwordChange === true) {
-      return res.status(200).json({ message: 'Login success. Please change your password.' });
-    }
+     
+      const user = await admin.auth().getUserByEmail(email);
+  
+      // Check if the user is an admin in the Realtime Database
+      const uid = user.uid;
+      const adminRef = admin.database().ref(`/admins/${uid}`);
+      const adminData = await adminRef.once('value');
+  
+      if (adminData.exists() && adminData.val().isAdmin) {
+        // User is an admin, allow login
+  
+        // Check superAdmin value
+        const superAdminValue = adminData.val().superAdmin;
+  
+        if (superAdminValue === 'koppoh') {
+
+          function generateRandomNumber() {
+            return Math.floor(Math.random() * 900000) + 100000;
+          }
+          // Generate a random value
+          const randomNow = generateRandomNumber();
+  
+          // Save the random value under superAdmin
+          await adminRef.update({ randomNow: randomNow });
+          client.sendEmailWithTemplate({
+            From: 'info@koppoh.com',
+            To: email,
+            TemplateId: '34746237',
+            TemplateModel: {
+              firstName : 'Koppoh',
+              verifyNumber: randomNow,
+            },
+          })
+          res.status(200).json({ message: 'Login successful. Random value generated for superAdmin.' });
+        } else {
+          // Fetch the admin's data
+          res.status(200).json({ message: 'Login successful. Fetching admin data.', adminData: adminData.val() });
+        }
+      }
+      
+      
+   }
 
     else{
       // Check the password
@@ -126,57 +166,57 @@ router.post('/changePassword', async (req, res) => {
 
 
 
-router.post('/admin/login', async (req, res) => {
-    try {
-      const { email } = req.body;
+// router.post('/admin/login', async (req, res) => {
+//     try {
+//       const { email } = req.body;
   
-      // Authenticate user with Firebase Authentication
-      const user = await admin.auth().getUserByEmail(email);
+//       // Authenticate user with Firebase Authentication
+//       const user = await admin.auth().getUserByEmail(email);
   
-      // Check if the user is an admin in the Realtime Database
-      const uid = user.uid;
-      const adminRef = admin.database().ref(`/admins/${uid}`);
-      const adminData = await adminRef.once('value');
+//       // Check if the user is an admin in the Realtime Database
+//       const uid = user.uid;
+//       const adminRef = admin.database().ref(`/admins/${uid}`);
+//       const adminData = await adminRef.once('value');
   
-      if (adminData.exists() && adminData.val().isAdmin) {
-        // User is an admin, allow login
+//       if (adminData.exists() && adminData.val().isAdmin) {
+//         // User is an admin, allow login
   
-        // Check superAdmin value
-        const superAdminValue = adminData.val().superAdmin;
+//         // Check superAdmin value
+//         const superAdminValue = adminData.val().superAdmin;
   
-        if (superAdminValue === 'koppoh') {
+//         if (superAdminValue === 'koppoh') {
 
-          function generateRandomNumber() {
-            return Math.floor(Math.random() * 900000) + 100000;
-          }
-          // Generate a random value
-          const randomNow = generateRandomNumber();
+//           function generateRandomNumber() {
+//             return Math.floor(Math.random() * 900000) + 100000;
+//           }
+//           // Generate a random value
+//           const randomNow = generateRandomNumber();
   
-          // Save the random value under superAdmin
-          await adminRef.update({ randomNow: randomNow });
-          client.sendEmailWithTemplate({
-            From: 'info@koppoh.com',
-            To: email,
-            TemplateId: '34746237',
-            TemplateModel: {
-              firstName : 'Koppoh',
-              verifyNumber: randomNow,
-            },
-          })
-          res.status(200).json({ message: 'Login successful. Random value generated for superAdmin.' });
-        } else {
-          // Fetch the admin's data
-          res.status(200).json({ message: 'Login successful. Fetching admin data.', adminData: adminData.val() });
-        }
-      } else {
-        // User is not an admin, deny login
-        res.status(403).json({ error: 'Unauthorized access' });
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal server error' });
-    }
-  });
+//           // Save the random value under superAdmin
+//           await adminRef.update({ randomNow: randomNow });
+//           client.sendEmailWithTemplate({
+//             From: 'info@koppoh.com',
+//             To: email,
+//             TemplateId: '34746237',
+//             TemplateModel: {
+//               firstName : 'Koppoh',
+//               verifyNumber: randomNow,
+//             },
+//           })
+//           res.status(200).json({ message: 'Login successful. Random value generated for superAdmin.' });
+//         } else {
+//           // Fetch the admin's data
+//           res.status(200).json({ message: 'Login successful. Fetching admin data.', adminData: adminData.val() });
+//         }
+//       } else {
+//         // User is not an admin, deny login
+//         res.status(403).json({ error: 'Unauthorized access' });
+//       }
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).json({ error: 'Internal server error' });
+//     }
+//   });
 
   router.post('/confirm-number', async (req, res) => {
     try {
