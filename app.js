4b70bdd3cdff88144ba5ba342d6868eddefa019f
koppohdotng <info.koppoh@gmail.com>
2024-04-08 10:19:44 +0100

    filtering

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const path = require('path');
 const puppeteer = require('puppeteer');
 var postmark = require("postmark");
 //const axios = require('axios');
+app.use(express.static('../'));
 var client = new postmark.ServerClient("61211298-3714-4551-99b0-1164f8a9cb33");
 const fs = require('fs');
 
