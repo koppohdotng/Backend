256908ed83453739d9fc3e2d16f865a90d3e3559
koppohdotng <info.koppoh@gmail.com>
2024-05-09 15:52:19 +0100

    same

--- investorRoutes/investorRoutes.js
+++ investorRoutes/investorRoutes.js
@@ -280,10 +280,10 @@ router.get('/confirm-verification', (req, res) => {
 
 
       console.log(typeof token)
-     token = parseFloat(token);
+     var tokenx = parseFloat(token);
 
       // Check if the token matches and the signupdate is within the last 30 minutes
-      if (token == storedToken) {
+      if (tokenx == storedToken) {
           const currentTimeInSeconds = Math.floor(Date.now() / 1000);
           const timeDifference = currentTimeInSeconds - signupdate;
           const thirtyMinutesInSeconds = 30 * 60; // 30 minutes in seconds
