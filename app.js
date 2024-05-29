e5f0c9dee2629105ee15b500559dd2c05847df48
lawal1 <info.koppoh@gmail.com>
2024-05-29 10:44:26 +0100

    same

--- app.js
+++ app.js
@@ -414,6 +414,29 @@ app.put('/api/update-user/:uid', (req, res) => {
 });
 
 
+app.post('/initialize-transaction', async (req, res) => {
+  const { email, amount } = req.body;
+  
+  const PAYSTACK_SECRET_KEY = 'sk_test_c33111b1192ff304809aa6f4889643e8d9677985';
+
+  try {
+    const response = await axios.post('https://api.paystack.co/transaction/initialize', {
+      email,
+      amount
+    }, {
+      headers: {
+        Authorization: `Bearer ${PAYSTACK_SECRET_KEY}`,
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const { data } = response;
+    res.json(data);
+  } catch (error) {
+    console.error('Error initializing transaction:', error.response ? error.response.data : error.message);
+    res.status(500).json({ error: 'Failed to initialize transaction' });
+  }
+});
 
 
  // Initialize Firebase Realtime Database
