43d306b2a62beba476a2650fa654bac041581183
lawal1 <info.koppoh@gmail.com>
2024-06-01 21:36:10 +0100

    same

--- investorRoutes/investorRoutes.js
+++ investorRoutes/investorRoutes.js
@@ -823,7 +823,7 @@ router.put('/update-dealcriteria/:uid', (req, res) => {
 
   // Update the deal criteria in the Firebase Realtime Database
   const db = admin.database();
-  const dealCriteriaRef = db.ref('users');
+  const dealCriteriaRef = db.ref('investors');
 
   dealCriteriaRef
       .child(userId)
