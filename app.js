9f83a2afd0592ab858e42fefa2683adce5c1c7d8
lawal1 <lhawhaltimi@gmail.com>
2024-07-15 13:47:20 +0100

    Initial commit

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const multer = require('multer');
 const path = require('path');
 const puppeteer = require('puppeteer');
 const { google } = require('googleapis');
-
+app.use(express.json());
 
 
 
