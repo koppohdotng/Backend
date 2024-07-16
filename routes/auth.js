53ba32caa99df93cb6316aa74c5b02a73cd814b2
lawal1 <lhawhaltimi@gmail.com>
2024-07-16 13:26:10 +0100

    Initial commit

--- routes/auth.js
+++ routes/auth.js
@@ -214,7 +214,7 @@ router.post('/signup', (req, res) => {
                 res.status(500).json({ error: 'Database error' });
               } else {
                 // Send email with confirmation link
-                const confirmationLink = `https://koppoh.ng/confirm-verification?email=${email}&token=${verificationToken}`;
+                const confirmationLink = `https://staging.koppoh.ng/confirm-verification?email=${email}&token=${verificationToken}`;
                 client.sendEmailWithTemplate({
                   From: 'info@koppoh.com',
                   To: email,
@@ -328,7 +328,7 @@ router.post('/resendVerification', async (req, res) => {
       .then((userRecord) => {
           // If user exists, resend verification email
           
-          const confirmationLink = `https://koppoh.ng/confirm-verification?email=${email}&token=${verificationToken}`;
+          const confirmationLink = `https://staging.koppoh.ng/confirm-verification?email=${email}&token=${verificationToken}`;
           console.log(confirmationLink)
           var firstName = userRecord.firstName
           
