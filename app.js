96d81bd2ce7b234669e71fd880ee67c3b2f2a360
lawal1 <lhawhaltimi@gmail.com>
2024-08-26 15:06:44 +0100

    fix

--- app.js
+++ app.js
@@ -511,7 +511,7 @@ app.post('/verifyTransactionFundingRequest/:userId', async (req, res) => {
       await dataRef.child(`${userId}/fundingRequest/${newKey}/payments`).set(paymentData);
 
       // Retrieve the user data to get the firstName
-      const userSnapshot = await dataRef.child(`${userId}`).once('value');
+      const userSnapshot = admin.database().ref(`/users/${userId}`).once('value');
       const userData = userSnapshot.val();
       console.log(userData)
       const firstName = userData.firstName;
