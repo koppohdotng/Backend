a676cb145cac393165bf9e624d7498277f9032a4
lawal1 <lhawhaltimi@gmail.com>
2024-08-26 13:13:27 +0100

    fix

--- routes/auth.js
+++ routes/auth.js
@@ -86,7 +86,7 @@ router.post('/signup', async (req, res) => {
               await usersRef.child(userRecord.uid).set(userData);
 
               // Generate confirmation link
-              const confirmationLink = `https://koppoh.ng/confirm-verification?email=${email}&token=${verificationToken}`;
+              const confirmationLink = `https://staging.koppoh.ng/confirm-verification?email=${email}&token=${verificationToken}`;
 
               // Send verification email
               await client.sendEmailWithTemplate({
@@ -221,7 +221,7 @@ router.post('/resendVerification', async (req, res) => {
       .then((userRecord) => {
           // If user exists, resend verification email
           
-          const confirmationLink = `https://koppoh.ng/confirm-verification?email=${email}&token=${verificationToken}`;
+          const confirmationLink = `https://staging.koppoh.ng/confirm-verification?email=${email}&token=${verificationToken}`;
           console.log(confirmationLink)
           var firstName = userRecord.firstName
           
