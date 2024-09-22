0fe64003b1fddd7d71c196ee96e8fee19019632c
lawal1 <lhawhaltimi@gmail.com>
2024-09-22 16:56:30 +0100

    fix

--- app.js
+++ app.js
@@ -2388,11 +2388,9 @@ app.get('/storeTeaser-pdf', async (req, res) => {
 
   try {
     const browser = await puppeteer.launch({
-      args: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-      ],
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
+    
     const page = await browser.newPage();
 
     // Increase the navigation timeout to 60 seconds
